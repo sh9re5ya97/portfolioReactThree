19588ae199910fd44ca93c3af38f593f657731c5
DESKTOP-RLFEEKI\Dell <singalshreya97@gmail.com>
2024-04-21 22:42:09 +0530

    contact,feedback,works

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-n0-repeat bg-center">
           <Navbar />
-          {/* <Hero /> */}
+          <Hero />
         </div>
-        {/* <About /> */}
-        {/* <Experience /> */}
-        {/* <Tech /> */}
-        {/* <Works /> */}
-        {/* <Feedbacks /> */}
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
